4390f0a0bd75e7a325d11c245caffc7e40fb09dc
Aaron Brown <ab341@comcast.net>
2015-06-26 08:45:16 -0700

    you can now save the post along with the image file to Parse

--- Makestagram-Swift-Solution-StarterProject/Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram-Swift-Solution-StarterProject/Makestagram/ViewControllers/TimelineViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class TimelineViewController: UIViewController {
     
@@ -27,7 +28,13 @@ class TimelineViewController: UIViewController {
     func takePhoto() {
         // instantiate photo taking class, provide callback for when photo  is selected
         photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
-            println("received a callback")
+            let imageData = UIImageJPEGRepresentation(image, 0.8)
+            let imageFile = PFFile(data: imageData)
+            imageFile.save()
+            
+            let post = PFObject(className: "Post")
+            post["imageFile"] = imageFile
+            post.save()
         }
     }
     
