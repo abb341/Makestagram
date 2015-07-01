40fb0f94af9e66efbc3a1a5b7030f1093389cfc3
Aaron Brown <ab341@comcast.net>
2015-07-01 09:29:46 -0700

    Now see a header for every post

---
+++ Makestagram-Swift-Solution-StarterProject/Makestagram/Timeline/PostSectionHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  PostSectionHeaderView.swift
+//  Makestagram
+//
+//  Created by Aaron Brown on 7/1/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PostSectionHeaderView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
